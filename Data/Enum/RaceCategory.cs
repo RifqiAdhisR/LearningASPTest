72e04491d3b7fc158280956f86a98407dbb48ef3
adhistira151200@gmail.com <A7118F1480@bangkit.academy>
2023-09-11 17:47:24 +0700

    03-Adding Model for Abstraction

---
+++ Data/Enum/RaceCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace WebLearningTest.Data.Enum
+{
+    public enum RaceCategory
+    {
+        Marthon,
+        Ultra,
+        Fivek,
+        Tenk,
+        HalfMarathon
+    }
+}
---
+++ Models/Address.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebLearningTest.Models
+{
+    public class Address
+    {
+        [Key]
+        public int id { get; set; }
+        public string street { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+    }
+}
